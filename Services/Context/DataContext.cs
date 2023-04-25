59eb1570a72e4f138bd20ba17141115207fc900e
Brand0nLe <93brandonle@gmail.com>
2023-04-25 10:22:12 -0700

    Set up file structure Models, DTO, Services, notes

---
+++ Services/Context/DataContext.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TaskTrackerBackend.Services.Context
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<UserModel> UserInfo { get; set; }
+        public DbSet<TaskItemModel> TaskInfo { get; set; }
+        public DataContext(DbContextOptions options): base(options){
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder){
+            base.OnModelCreating(builder);
+        }
+        
+    }
+}
