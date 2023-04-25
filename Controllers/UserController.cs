13d342ef5761b4e876a9213bf4825c13e7d3849e
Brand0nLe <93brandonle@gmail.com>
2023-04-25 12:00:03 -0700

    Connecting User Controller and User Service

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using TaskTrackerBackend.Models.DTO;
+using TaskTrackerBackend.Services;
 
 namespace TaskTrackerBackend.Controllers
 {
@@ -10,6 +12,26 @@ namespace TaskTrackerBackend.Controllers
     [Route("[controller]")]
     public class UserController : ControllerBase
     {
-        
+
+        private readonly UserService _data;
+        public UserController(UserService dataFromService)
+        {
+            _data = dataFromService;
+        }
+        //Login
+
+        //Add a user
+            //if user already exists
+            //if they do not exist we can then have the acc created
+            //else throw a false
+            
+            public bool AddUser(CreateAccountDTO UserToAdd)
+            {
+                return _data.AddUser(UserToAdd);
+            }
+
+        //Update user account
+
+        //Delete user account
     }
 }
--- Services/UserService.cs
+++ Services/UserService.cs
@@ -2,11 +2,33 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TaskTrackerBackend.Models.DTO;
+using TaskTrackerBackend.Services.Context;
 
 namespace TaskTrackerBackend.Services
 {
     public class UserService
     {
-        
+        private readonly DataContext _context;
+        public UserService(DataContext context)
+        {
+            _context = context;
+        }
+
+        public bool DoesUserExist(string Username)
+        {
+            //check the table to see if the username exists
+            //if 1 item matches the condition we will return the item
+            //if no item matches the ocnditiion it will return null
+            //if multiple items match then error will occur
+            return _context.UserInfo.SingleOrDefault(user => user.UserName == Username)
+
+        }
+        public bool AddUser(CreateAccountDTO UserToAdd)
+        {
+            //if user already exists
+            //if they do not exist then create acc
+            //else throw a false
+        }
     }
 }
